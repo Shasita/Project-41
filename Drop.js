dc7ef0d04c66da0c9a2402c4a6c7733162308285
Shasita <72453538+Shasita@users.noreply.github.com>
2021-05-15 12:40:48 +0530

    Add files via upload

---
+++ Drop.js
@@ -0,0 +1,26 @@
+class Drop {
+   constructor(x, y){
+    var options = {
+        friction: 0.001,
+        restitution: 0.1
+    }
+    this.rain = Bodies.circle(x, y, 5, options);
+    this.r = 5;
+    World.add(world, this.rain);
+   }
+
+   updateY(){     
+    if(this.rain.position.y > height){
+
+        Matter.Body.setPosition(this.rain, {x:random(0,400), y:random(0,400)})
+    }
+}
+
+showDrop(){
+    var pos = this.rain.position;
+    fill("blue")
+    ellipseMode(CENTER);
+    ellipse(pos.x,pos.y,this.r,this.r);
+}
+
+}
